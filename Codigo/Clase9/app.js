5b8ff7a5b8ae5f44321290d343f8489830c8c702
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-09-21 23:10:30 -0300

    js

---
+++ Codigo/Clase9/app.js
@@ -0,0 +1,67 @@
+// interacciones
+
+
+
+//prompt
+/* let nombre = prompt ("Decime tu nombre") */
+
+//alert
+
+/* alert ("Hola "+nombre) */
+
+// confirm
+/* confirm (nombre + " "+ "Queres seguir en esta pagina?") */
+
+// javascript
+/* Interpretado
+orientado a objetos
+imperativo
+case sensitive = 
+let hola = "Saludo"
+let Hola = "Saludo2"
+
+tipado Debil
+
+lenguaje dinamico
+
+let numero1;
+numero1= 34
+numero1= "texto"
+document.write (numero1)
+
+*/
+/* let nombre= prompt ("Decime Tu nombre")
+let num1 = Number(prompt ("Decime un numero"))
+let num2 = Number(prompt ("Decime otro numero")) */
+
+/* parseInt
+parseFloat */
+
+/* document.write (nombre + " el resultado de tu suma es: " + (num1+num2)) */
+
+
+/* plantillas literales
+template literales
+backstick */
+/* 
+document.write (`nombre ${nombre} el resultado de tu suma es ${num1+num2}` ) */
+
+/* operadores */
+
+let num1 = 24
+let num2= "24"
+let num3=30
+let num4=30
+let num5= 21
+let num6=3
+
+console.log(num1+num3);  // suma
+console.log(num3-num5); // resta
+console.log(num1*num3); //multiplicacion
+console.log(num1/num3); // division
+console.log(num5%num6); // modulo
+
+console.log("hola");
+
+
+
